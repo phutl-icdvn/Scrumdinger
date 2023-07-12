884c1c20c801410019486c605bef0eedaed4374c
longphu753951 <51697950+longphu753951@users.noreply.github.com>
2023-07-13 06:35:00 +0700

    add metting header

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                Circle().strokeBorder(lineWidth: 24, antialiased: true)
+                HStack {
+                    Text("Speaker of 3")
+                    Spacer()
+                    Button(action:{}) {
+                        Image(systemName: "forward.fill")
+                    }
+                }
+            }
+                
+            
+        }
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .padding()
+        
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
